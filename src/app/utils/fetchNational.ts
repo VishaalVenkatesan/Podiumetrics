d25a0db33d004c0aecd7b71dc468e5cff97c518c
Vishaal Venkatesan <vishaal@Vishaals-Mac.local>
2024-04-13 20:49:09 +0530

    flag condition updated

--- src/app/utils/fetchNational.ts
+++ src/app/utils/fetchNational.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const fetchNational = async (nationality: string) => {
-    const nationalityIndex = nationality === 'American' ? 1 : 0;
+    const nationalityIndex = nationality === 'American' ? 3 : (nationality === 'French' ? 2 : 0);
     try {
         const response = await axios.get(`https://restcountries.com/v3.1/demonym/${nationality}`);
         return response.data[nationalityIndex].flags; // Access the flags of the first country
