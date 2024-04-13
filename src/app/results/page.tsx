a82fe033ebb662323dde33f266fc8625d1d4043a
Vishaal Venkatesan <vishaal@Vishaals-Mac.local>
2024-04-13 20:29:38 +0530

    components updated

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -35,7 +35,7 @@
 // }
 
 
-// const Page = () => {
+const Page = () => {
 //   const xml2js = require('xml2js');
 //   const currentYear = new Date().getFullYear();
 //   const [year, setYear] = useState(currentYear.toString());
@@ -119,85 +119,85 @@
 
 
 
-//   return (
-//     <div className="">
-//     <form
-//         onSubmit={handleSubmit}
-//         className="flex flex-col max-w-md p-8 mx-auto rounded-lg shadow-md gap-y-6"
-//       >
-//            <select 
-//                 value={year} 
-//                 onChange={(e) => {setYear(e.target.value)}}
-//                 className='w-full p-2 font-mono text-xl font-bold text-center rounded-md focus:outline-none focus:shadow-outline'
-//                 >
-//               {Array.from({length: new Date().getFullYear() - 1950 + 1}, (_, i) => new Date().getFullYear() - i).map((year) => (
-//               <option key={year} value={year}>{year}</option>
-//                ))}
-//             </select>
-//           <select
-//            id="race"
-//            value={selectedRace ? selectedRace.round : ''}
-//            onChange={handleRaceChange}
-//            name='round' 
-//            className='w-full p-4 font-mono font-bold text-center rounded-md focus:outline-none focus:shadow-outline'
-//           >
-//             {circuits.map((race) => (
-//               <option  key={race.round} value={race.round}>
-//                 {race.RaceName[0]} ({race.Circuit[0].CircuitName[0]})
-//               </option>
-//            ))}
-//           </select>
-//         <button
-//           type="submit"
-//           className="py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-//         >
-//           Submit
-//         </button>
-//       </form>
-//   <div>
-//     {loading ? (
-//       <LoadingSpinner />
-//     ) : error ? (
-//       <div>{error}</div>
-//     ) : selectedRace ? (
-//       <Table>
-//         <TableHeader>
-//           <TableColumn>Position</TableColumn>
-//           <TableColumn>Points</TableColumn>
-//           <TableColumn>Permanent Number</TableColumn>
-//           <TableColumn>Given Name</TableColumn>
-//           <TableColumn>Family Name</TableColumn>
-//           <TableColumn>Name</TableColumn>
-//           <TableColumn>Grid</TableColumn>
-//           <TableColumn>Laps</TableColumn>
-//           <TableColumn>Status</TableColumn>
-//           <TableColumn>Time</TableColumn>
-//           <TableColumn>Fastest Lap Time</TableColumn>
-//           <TableColumn>Average Speed</TableColumn>
-//         </TableHeader>
-//         <TableBody>
-//   {selectedRace.map((race: Race, index: number) => (
-//     <TableRow key={index}>
-//       <TableCell>{race.position}</TableCell>
-//       <TableCell>{race.points}</TableCell>
-//       <TableCell>{race.Driver.GivenName}</TableCell>
-//       <TableCell>{race.Driver.FamilyName}</TableCell>
-//       <TableCell>{race.Driver.Nationality}</TableCell>
-//       <TableCell>{race.Constructor.name}</TableCell>  
-//       <TableCell>{race.Grid}</TableCell>
-//       <TableCell>{race.Laps}</TableCell>
-//       <TableCell>{race.Status}</TableCell>
-//               <TableCell>{race.Time}</TableCell>
-//               <TableCell>{race.FastestLap.Time}</TableCell>
-//               <TableCell>{race.FastestLap.AverageSpeed}</TableCell>
-//             </TableRow>
-//           ))}
-//         </TableBody>
-//       </Table>
-//     ) : null}
-//   </div>
-//   </div>
-// );
-// }
+  return (
+    <div className="">
+    {/* <form
+        onSubmit={handleSubmit}
+        className="flex flex-col max-w-md p-8 mx-auto rounded-lg shadow-md gap-y-6"
+      >
+           <select 
+                value={year} 
+                onChange={(e) => {setYear(e.target.value)}}
+                className='w-full p-2 font-mono text-xl font-bold text-center rounded-md focus:outline-none focus:shadow-outline'
+                >
+              {Array.from({length: new Date().getFullYear() - 1950 + 1}, (_, i) => new Date().getFullYear() - i).map((year) => (
+              <option key={year} value={year}>{year}</option>
+               ))}
+            </select>
+          <select
+           id="race"
+           value={selectedRace ? selectedRace.round : ''}
+           onChange={handleRaceChange}
+           name='round' 
+           className='w-full p-4 font-mono font-bold text-center rounded-md focus:outline-none focus:shadow-outline'
+          >
+            {circuits.map((race) => (
+              <option  key={race.round} value={race.round}>
+                {race.RaceName[0]} ({race.Circuit[0].CircuitName[0]})
+              </option>
+           ))}
+          </select>
+        <button
+          type="submit"
+          className="py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+        >
+          Submit
+        </button>
+      </form>
+  <div>
+    {loading ? (
+      <LoadingSpinner />
+    ) : error ? (
+      <div>{error}</div>
+    ) : selectedRace ? (
+      <Table>
+        <TableHeader>
+          <TableColumn>Position</TableColumn>
+          <TableColumn>Points</TableColumn>
+          <TableColumn>Permanent Number</TableColumn>
+          <TableColumn>Given Name</TableColumn>
+          <TableColumn>Family Name</TableColumn>
+          <TableColumn>Name</TableColumn>
+          <TableColumn>Grid</TableColumn>
+          <TableColumn>Laps</TableColumn>
+          <TableColumn>Status</TableColumn>
+          <TableColumn>Time</TableColumn>
+          <TableColumn>Fastest Lap Time</TableColumn>
+          <TableColumn>Average Speed</TableColumn>
+        </TableHeader>
+        <TableBody>
+  {selectedRace.map((race: Race, index: number) => (
+    <TableRow key={index}>
+      <TableCell>{race.position}</TableCell>
+      <TableCell>{race.points}</TableCell>
+      <TableCell>{race.Driver.GivenName}</TableCell>
+      <TableCell>{race.Driver.FamilyName}</TableCell>
+      <TableCell>{race.Driver.Nationality}</TableCell>
+      <TableCell>{race.Constructor.name}</TableCell>  
+      <TableCell>{race.Grid}</TableCell>
+      <TableCell>{race.Laps}</TableCell>
+      <TableCell>{race.Status}</TableCell>
+              <TableCell>{race.Time}</TableCell>
+              <TableCell>{race.FastestLap.Time}</TableCell>
+              <TableCell>{race.FastestLap.AverageSpeed}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    ) : null}
+  </div> */}
+  </div>
+);
+}
 
-// export default Page;
+export default Page;
