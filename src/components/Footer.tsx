2eaa7d7b1eee16ff644b12320d532a11a354d31d
Vishaal Venkatesan <vishaal@Vishaals-Mac.local>
2024-04-13 20:56:38 +0530

    navbar updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
-    <div className="mb-[20px] mt-auto">
-      <h2 className="text-center ">© 2024 Podiumetrics dream big</h2>
+    <div className="flex flex-row justify-between mt-auto bg-blue-900 md:h-[60px] h-[30px] md:p-4 opacity-50 p-1">
+      <h2 className="text-center pl-[20px] font-mutuka md:text-xl">© 2024 Podiumetrics</h2>
+      <h2 className="pr-[20px] font-mutuka md:text-xl">By Vishaal Venkatesan</h2>
+      
       </div>
   )
 }
